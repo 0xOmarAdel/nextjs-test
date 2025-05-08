6be927cdd965506aa7a11568987e62ca0b8d24c6
Omar Adel <omaradelawad@gmail.com>
2025-05-08 18:09:57 +0300

    feat: Revalidate home page after post submission

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import axios from "axios";
+import { revalidatePath } from "next/cache";
 
 export const createPost = async ({
   title,
@@ -9,11 +10,13 @@ export const createPost = async ({
   title: string;
   content: string;
 }) => {
-  const post = await axios({
+  await axios({
     method: "POST",
     url: "http://161.97.126.23:3001/api/posts",
     data: { title, content },
   });
+
+  revalidatePath("/");
 
   return null;
 };
